316ea78d1909754f9442cb6feec2256c97b025b2
Daniel Fidalgo <daniel.fidalgo@skipthedishes.ca>
2021-03-25 13:00:48 -0500

    master - action fix

--- primebid-preact/tests/header.test.tsx
+++ primebid-preact/tests/header.test.tsx
@@ -2,10 +2,11 @@ import { h } from 'preact';
 import Header from '../src/components/header';
 // See: https://github.com/preactjs/enzyme-adapter-preact-pure
 import { shallow } from 'enzyme';
+import { UserState } from '../src/services/UserState';
 
 describe('Initial Test of the Header', () => {
     test('Header renders 3 nav items', () => {
-        const context = shallow(<Header />);
+        const context = shallow(<Header user={new UserState(null)} />);
         expect(context.find('h1').text()).toBe('Preact App');
         expect(context.find('Link').length).toBe(3);
     });
